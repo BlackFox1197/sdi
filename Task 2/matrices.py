296033158ae63b72e120cb2625876311d1428c2b
Daniel Lenhardt <ckwdani@gmail.com>
2021-01-18 19:02:34 +0100

    add combine measures

--- Task 2/matrices.py
+++ Task 2/matrices.py
@@ -8,24 +8,39 @@ s1s2 = ["SOPID", "PizzaName", "nurishment", "total", "CID", "PIID", "FirstName",
 
 
 matEdit = nltk.edit_distance(mediated, s1s2)
-#newMat = np.arange(mediated)
+
+
 lev=np.zeros((len(mediated), len(s1s2)))
-#print(newMat)
 for wordI in range (0, len(mediated)):
 	for wordJ in range (0, len(s1s2)):
 		lev[wordI, wordJ] = nltk.edit_distance(mediated[wordI], s1s2[wordJ])
 
 jaro=np.zeros((len(mediated), len(s1s2)))
-#print(newMat)
 for wordI in range (0, len(mediated)):
 	for wordJ in range (0, len(s1s2)):
 		jaro[wordI, wordJ] = jaro_similarity(mediated[wordI], s1s2[wordJ])
 
 jaccard=np.zeros((len(mediated), len(s1s2)))
-#print(newMat)
 for wordI in range (0, len(mediated)):
 	for wordJ in range (0, len(s1s2)):
 		jaccard[wordI, wordJ] = nltk.jaccard_distance(set(mediated[wordI]), set(s1s2[wordJ]))
 
 
-print(jaccard)
+minimum=np.zeros((len(mediated), len(s1s2)))
+for wordI in range (0, len(mediated)):
+	for wordJ in range (0, len(s1s2)):
+		minimum[wordI, wordJ] = min(lev[wordI, wordJ], jaro[wordI, wordJ], jaccard[wordI, wordJ])
+
+maximum=np.zeros((len(mediated), len(s1s2)))
+for wordI in range (0, len(mediated)):
+	for wordJ in range (0, len(s1s2)):
+		maximum[wordI, wordJ] = max(lev[wordI, wordJ], jaro[wordI, wordJ], jaccard[wordI, wordJ])
+
+
+average=np.zeros((len(mediated), len(s1s2)))
+for wordI in range (0, len(mediated)):
+	for wordJ in range (0, len(s1s2)):
+		average[wordI, wordJ] = np.mean([lev[wordI, wordJ], jaro[wordI, wordJ], jaccard[wordI, wordJ]])
+
+
+print(average)
