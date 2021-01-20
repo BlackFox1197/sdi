560a2f97efb86f8ee8932b9ca36ab68afa72a062
CelesMaCa <CelesMaCa@gmail.com>
2021-01-20 16:37:57 +0100

    Changing the computation of Edit Distance

--- Task 2/matrices.py
+++ Task 2/matrices.py
@@ -2,26 +2,33 @@ import nltk
 from nltk.metrics.distance import jaro_similarity
 import numpy as np
 
+# Defining mediated and datasource attributes
 mediated = ["UID", "name", "CCInfo", "PID", "OID", "OID", "UID", "orderNumber", "totalCost", "Adress", "PID", "pizzaName", "nutrition", "price"]
-
-s1s2 = ["SOPID", "PizzaName", "nurishment", "total", "CID", "PIID", "FirstName", "LastName", "PIID", "CreditCardNumber", "ccv", "expDate", "OID", "CID", "orderCode", "totalAmount", "Adress", "PID", "OID", "PID", "title", "nurishment", "price", "UID", "FirstName", "LastName", "CCNumber", "ccv", "expDate", "OID", "UID", "orderNumber", "Sum", "AID", "OID", "PID", "AID", "User", "street", "city", "zip"]
+datasource = ["SOPID", "PizzaName", "nurishment", "total", "CID", "PIID", "FirstName", "LastName", "PIID", "CreditCardNumber", "ccv", "expDate", "OID", "CID", "orderCode", "totalAmount", "Adress", "PID", "OID", "PID", "title", "nurishment", "price", "UID", "FirstName", "LastName", "CCNumber", "ccv", "expDate", "OID", "UID", "orderNumber", "Sum", "AID", "OID", "PID", "AID", "User", "street", "city", "zip"]
 
 log = open("Task 2/matrixLog.txt", "w")
 
+# Computing the length datasource attribute with the most characters
+maxLength = 0
+for i in range (0, len(datasource)):
+	if maxLength < len(datasource[i]):
+		maxLength = len(datasource[i])
+log.write("Maximum length of an datasource attribute: " + str(maxLength))
+
 # Computing Edit-Distance-Matrix
-lev=np.zeros((len(mediated), len(s1s2)))
+lev=np.zeros((len(mediated), len(datasource)))
 for wordI in range (0, len(mediated)):
-	for wordJ in range (0, len(s1s2)):
-		lev[wordI, wordJ] = nltk.edit_distance(mediated[wordI], s1s2[wordJ])
+	for wordJ in range (0, len(datasource)):
+		lev[wordI, wordJ] = 1 - nltk.edit_distance(mediated[wordI], datasource[wordJ]) / maxLength
 
 # Logging Edit-Distance-Matrix
-log.write("Edit-Distance-Matrix: \n")
+log.write("\n\nEdit-Distance-Matrix: \n")
 log.write("[")
 for wordI in range (0, len(mediated)):
 	log.write("(")
-	for wordJ in range (0, len(s1s2)):
+	for wordJ in range (0, len(datasource)):
 		log.write("" + str(lev[wordI, wordJ]))
-		if wordJ != (len(s1s2) - 1):
+		if wordJ != (len(datasource) - 1):
 			log.write(", ")
 	log.write(")")
 	if wordI != (len(mediated) - 1):
@@ -31,19 +38,19 @@ for wordI in range (0, len(mediated)):
 
 
 # Computing Jaro-Matrix
-jaro=np.zeros((len(mediated), len(s1s2)))
+jaro=np.zeros((len(mediated), len(datasource)))
 for wordI in range (0, len(mediated)):
-	for wordJ in range (0, len(s1s2)):
-		jaro[wordI, wordJ] = jaro_similarity(mediated[wordI], s1s2[wordJ])
+	for wordJ in range (0, len(datasource)):
+		jaro[wordI, wordJ] = jaro_similarity(mediated[wordI], datasource[wordJ])
 
 # Logging Jaro-Matrix
 log.write("\n\nJaro-Matrix: \n")
 log.write("[")
 for wordI in range (0, len(mediated)):
 	log.write("(")
-	for wordJ in range (0, len(s1s2)):
+	for wordJ in range (0, len(datasource)):
 		log.write("" + str(jaro[wordI, wordJ]))
-		if wordJ != (len(s1s2) - 1):
+		if wordJ != (len(datasource) - 1):
 			log.write(", ")
 	log.write(")")
 	if wordI != (len(mediated) - 1):
@@ -52,19 +59,19 @@ for wordI in range (0, len(mediated)):
 		log.write("]")
 
 # Computing Jaccard-Matrix
-jaccard=np.zeros((len(mediated), len(s1s2)))
+jaccard=np.zeros((len(mediated), len(datasource)))
 for wordI in range (0, len(mediated)):
-	for wordJ in range (0, len(s1s2)):
-		jaccard[wordI, wordJ] = nltk.jaccard_distance(set(mediated[wordI]), set(s1s2[wordJ]))
+	for wordJ in range (0, len(datasource)):
+		jaccard[wordI, wordJ] = nltk.jaccard_distance(set(mediated[wordI]), set(datasource[wordJ]))
 
 # Logging Jaccard-Matrix
 log.write("\n\nJaccard-Matrix: \n")
 log.write("[")
 for wordI in range (0, len(mediated)):
 	log.write("(")
-	for wordJ in range (0, len(s1s2)):
+	for wordJ in range (0, len(datasource)):
 		log.write("" + str(jaccard[wordI, wordJ]))
-		if wordJ != (len(s1s2) - 1):
+		if wordJ != (len(datasource) - 1):
 			log.write(", ")
 	log.write(")")
 	if wordI != (len(mediated) - 1):
@@ -73,9 +80,9 @@ for wordI in range (0, len(mediated)):
 		log.write("]")
 
 # Computing Minimum-Combined-Matrix
-minimum=np.zeros((len(mediated), len(s1s2)))
+minimum=np.zeros((len(mediated), len(datasource)))
 for wordI in range (0, len(mediated)):
-	for wordJ in range (0, len(s1s2)):
+	for wordJ in range (0, len(datasource)):
 		minimum[wordI, wordJ] = min(lev[wordI, wordJ], jaro[wordI, wordJ], jaccard[wordI, wordJ])
 
 # Logging Minimum-Combined-Matrix
@@ -83,9 +90,9 @@ log.write("\n\nMinimum-Combined-Matrix: \n")
 log.write("[")
 for wordI in range (0, len(mediated)):
 	log.write("(")
-	for wordJ in range (0, len(s1s2)):
+	for wordJ in range (0, len(datasource)):
 		log.write("" + str(minimum[wordI, wordJ]))
-		if wordJ != (len(s1s2) - 1):
+		if wordJ != (len(datasource) - 1):
 			log.write(", ")
 	log.write(")")
 	if wordI != (len(mediated) - 1):
@@ -94,9 +101,9 @@ for wordI in range (0, len(mediated)):
 		log.write("]")
 
 # Computing Maximum-Combined-Matrix
-maximum=np.zeros((len(mediated), len(s1s2)))
+maximum=np.zeros((len(mediated), len(datasource)))
 for wordI in range (0, len(mediated)):
-	for wordJ in range (0, len(s1s2)):
+	for wordJ in range (0, len(datasource)):
 		maximum[wordI, wordJ] = max(lev[wordI, wordJ], jaro[wordI, wordJ], jaccard[wordI, wordJ])
 
 # Logging Maximum-Combined-Matrix
@@ -104,9 +111,9 @@ log.write("\n\nMaximum-Combined-Matrix: \n")
 log.write("[")
 for wordI in range (0, len(mediated)):
 	log.write("(")
-	for wordJ in range (0, len(s1s2)):
+	for wordJ in range (0, len(datasource)):
 		log.write("" + str(maximum[wordI, wordJ]))
-		if wordJ != (len(s1s2) - 1):
+		if wordJ != (len(datasource) - 1):
 			log.write(", ")
 	log.write(")")
 	if wordI != (len(mediated) - 1):
@@ -115,9 +122,9 @@ for wordI in range (0, len(mediated)):
 		log.write("]")
 
 # Computing Average-Combined-Matrix
-average=np.zeros((len(mediated), len(s1s2)))
+average=np.zeros((len(mediated), len(datasource)))
 for wordI in range (0, len(mediated)):
-	for wordJ in range (0, len(s1s2)):
+	for wordJ in range (0, len(datasource)):
 		average[wordI, wordJ] = np.mean([lev[wordI, wordJ], jaro[wordI, wordJ], jaccard[wordI, wordJ]])
 
 # Logging Average-Combined-Matrix
@@ -125,9 +132,9 @@ log.write("\n\nAverage-Combined-Matrix: \n")
 log.write("[")
 for wordI in range (0, len(mediated)):
 	log.write("(")
-	for wordJ in range (0, len(s1s2)):
+	for wordJ in range (0, len(datasource)):
 		log.write("" + str(average[wordI, wordJ]))
-		if wordJ != (len(s1s2) - 1):
+		if wordJ != (len(datasource) - 1):
 			log.write(", ")
 	log.write(")")
 	if wordI != (len(mediated) - 1):
